49656c08eb705aa623380a60f03a9e1a5e2529e4
gb34 <gb34@uw.edu>
2020-11-21 03:19:23 -0800

    Having issues with building unit tests

--- test/apptest.js
+++ test/apptest.js
@@ -6,18 +6,20 @@ let server = require("../app");
 
 //Our parent block
 describe("Start running tests", () => {
-  // describe("/GET /shoot/:play/:player_name", () => {
-  //     it("it should test for the right input parameter", (done) => {
-  //     chai.request(server)
-  //       .get("/shoot/:play/:player_name")
-  //       .end((err, res) => {
-  //             (res).should.have.status(200);
-  //             (res.body).should.be.a("text");
-  //             (res.body.podcasts.length).should.be.eql(1);
-  //             done();
-  //          });
-  //       });
-  //  });
+  describe("/GET /shoot/:play/:player_name", () => {
+      it("it should test for the right input parameter", (done) => {
+      chai.request(server)
+        .get("/shoot/:play/:player_name")
+        .query({play: 'rock', player_name: 'ghis'})
+        .end((err, res) => {
+              console.log(res.body);
+              (res).should.have.status(200);
+              (res.body).should.be.a("text");
+              // (res.body).should.be.eql("ghis wins the round");
+              done();
+           });
+        });
+   });
 
   describe("/GET leaderboard", () => {
         it("it should return an object of winners", (done) => {
